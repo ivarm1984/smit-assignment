8ae8cbe4b0b4233753b42f8750435d1d5cc6d8ab
Ivar Mällas <ivar.mallas@gmail.com>
2025-01-04 16:06:28 +0200

    basic front navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {SearchComponent} from './search/search.component';
+import {BookActivityComponent} from './book-activity/book-activity.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {
+      path: '',
+      title: 'Search',
+      component: SearchComponent,
+    },
+    {
+      path: 'book-activity',
+      title: 'Book Activity',
+      component: BookActivityComponent,
+    },];
---
+++ frontend/src/app/book-activity/book-activity.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-activity',
+  imports: [],
+  templateUrl: './book-activity.component.html',
+  styleUrl: './book-activity.component.css'
+})
+export class BookActivityComponent {
+
+}
